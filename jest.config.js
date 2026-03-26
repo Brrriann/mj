b25c5c4a7e87aa71b812b13f4556f7a390763dc3
명진탐정사무소 <dev@mjdet.kr>
2026-03-26 12:16:00 +0900

    fix: correct jest setupFilesAfterEnv key and align eslint-config-next version

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
-  setupFilesAfterEach: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
